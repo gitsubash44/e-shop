22ed254ae61406ce07778c436cd944b0faf362b3
gitsubash44 <dhamisubash866@gmail.com>
2024-11-16 21:05:57 +0545

    adding

--- E_Shop/E_Shop/settings.py
+++ E_Shop/E_Shop/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'E_Shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
